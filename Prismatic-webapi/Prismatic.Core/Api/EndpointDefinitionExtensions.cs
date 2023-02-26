b9995639e8deb922e0c01ef2db88062b864b53dc
João Xavier <fastjonh@gmail.com>
2023-02-25 22:19:21 -0300

    adding endpoint definition core class

---
+++ Prismatic-webapi/Prismatic.Core/Api/EndpointDefinitionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Prismatic.Core.Api
+{
+    public static class EndpointDefinitonExtensions
+    {
+        public static void UseCustomExceptionHandler(this WebApplication app)
+        {
+            app.UseExceptionHandler(exceptionHandlerApp =>
+            {
+                exceptionHandlerApp.Run(async context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                    context.Response.ContentType = System.Net.Mime.MediaTypeNames.Application.Json;
+
+                    var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>()!;
+
+                    var json = JsonConvert.SerializeObject(exceptionHandlerPathFeature.Error, Formatting.Indented);
+
+                    await context.Response.WriteAsync(json);
+                });
+            });
+        }
+
+        public static void UseEndpointDefinitions(this WebApplication app, params Type[] types)
+        {
+            var definitions = types
+                .Select(t => GetEndpointsTypes(t)
+                .Select(Activator.CreateInstance)
+                .Cast<IEndpointDefinition>())
+                .SelectMany(d => d);
+
+            foreach (var endpointDefinition in definitions)
+            {
+                endpointDefinition.DefineEndpoints(app);
+            }
+        }
+
+        private static IEnumerable<Type> GetEndpointsTypes(Type type)
+        {
+            return type.Assembly.ExportedTypes.Where(x => typeof(IEndpointDefinition).IsAssignableFrom(x) && !x.IsInterface);
+        }
+    }
+}
---
+++ Prismatic-webapi/Prismatic.Core/Api/IEndpointDefinition.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Prismatic.Core.Api
+{
+    public interface IEndpointDefinition
+    {
+        void DefineEndpoints(WebApplication app);
+    }
+}
