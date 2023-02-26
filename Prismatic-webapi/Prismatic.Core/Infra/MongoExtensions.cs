1493d4e9ae9bda7c6560174bfcc149be6868f486
João Xavier <fastjonh@gmail.com>
2023-02-26 03:30:29 -0300

    adding exception message to env variable

--- Prismatic-webapi/Prismatic.Core/Infra/MongoExtensions.cs
+++ Prismatic-webapi/Prismatic.Core/Infra/MongoExtensions.cs
@@ -8,8 +8,13 @@ namespace Prismatic.Core.Infra
     {
         public static void ConfigureMongoClient(this WebApplicationBuilder builder)
         {
-            var mongoPassword = Environment.GetEnvironmentVariable("MONGO_PASSWORD");
-            builder.Services.AddMongoClient(builder.Configuration.GetConnectionString("MongoDb").Replace("MONGO_PASSWORD", mongoPassword));
+            var mongoPassword = 
+                Environment.GetEnvironmentVariable("MONGO_PASSWORD") 
+                ?? 
+                throw new Exception("Environment Variable MONGO_PASSWORD not declared");
+
+            var connectionString = builder.Configuration.GetConnectionString("MongoDb");
+            builder.Services.AddMongoClient(connectionString.Replace("MONGO_PASSWORD", mongoPassword));
         }
     }
 }
