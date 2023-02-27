886fbf59cc52140f2f52db09ab41e32fca58e094
João Xavier <fastjonh@gmail.com>
2023-02-27 04:20:33 -0300

    added class to implement endpoints

---
+++ Prismatic-webapi/Prismatic.API/Endpoints/Matches/StartMatch.cs
@@ -0,0 +1,18 @@
+﻿using Prismatic.Core.Api;
+using Prismatic.Domain.Interfaces;
+
+namespace Prismatic.API.Endpoints.Matches
+{
+    public class StartMatch : IEndpointDefinition
+    {
+        public void DefineEndpoints(WebApplication app)
+        {
+
+        }
+
+        public Task Handler(IMatchRepository matchRepository)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Prismatic-webapi/Prismatic.API/Endpoints/Players/CreatePlayer.cs
@@ -0,0 +1,18 @@
+﻿using Prismatic.Core.Api;
+using Prismatic.Domain.Interfaces;
+
+namespace Prismatic.API.Endpoints.Players
+{
+    public class CreatePlayer : IEndpointDefinition
+    {
+        public void DefineEndpoints(WebApplication app)
+        {
+
+        }
+
+        public Task Handler(IMatchRepository matchRepository)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
