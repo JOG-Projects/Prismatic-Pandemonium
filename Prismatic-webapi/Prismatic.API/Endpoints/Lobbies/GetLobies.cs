62efd47da0b6b83821c5ff27c451a8e90ebd9376
João Xavier <fastjonh@gmail.com>
2023-02-27 04:18:40 -0300

    creating join and get lobbies endpoints

---
+++ Prismatic-webapi/Prismatic.API/Endpoints/Lobbies/GetLobies.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Prismatic.Application.Lobbies;
+using Prismatic.Core.Api;
+using Prismatic.Domain.Entities;
+
+namespace Prismatic.API.Endpoints.Lobbies
+{
+    public class GetLobbies : IEndpointDefinition
+    {
+        public void DefineEndpoints(WebApplication app)
+        {
+            app.MapGet("lobby", Handler).WithOpenApi();
+        }
+
+        public async Task<List<Lobby>> Handler(IMediator mediator)
+        {
+            return await mediator.Send(new GetLobbiesRequest());
+        }
+    }
+}
