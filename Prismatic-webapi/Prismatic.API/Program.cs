2574a680980e340dbc39c89b26767e077b59ddb9
João Xavier <fastjonh@gmail.com>
2023-02-27 04:21:07 -0300

    adding mediatR and hubs configuration to program

--- Prismatic-webapi/Prismatic.API/Program.cs
+++ Prismatic-webapi/Prismatic.API/Program.cs
@@ -1,4 +1,5 @@
 using Prismatic.API;
+using Prismatic.Application;
 using Prismatic.Core.Api;
 using Prismatic.Core.Infra;
 
@@ -8,7 +9,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 builder.Services.AddSignalR();
-
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining(typeof(HandlersAssemblyMark)));
 builder.Services.ConfigurePrismaticServices();
 builder.ConfigureMongoClient();
 
@@ -23,5 +24,6 @@ if (app.Environment.IsDevelopment())
 app.UseEndpointDefinitions();
 app.UseCustomExceptionHandler();
 app.UseCors(o => o.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UsePrismaticHubs();
 
 app.Run();
