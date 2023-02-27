19b903fc38923bc4b40fa6f15011ea19b88144bb
João Xavier <fastjonh@gmail.com>
2023-02-27 04:17:10 -0300

    creating repositories

--- Prismatic-webapi/Prismatic.Infra/Repositories/MatchRepository.cs
+++ Prismatic-webapi/Prismatic.Infra/Repositories/MatchRepository.cs
@@ -3,12 +3,10 @@ using Prismatic.Core.Infra;
 using Prismatic.Domain.Entities;
 using Prismatic.Domain.Interfaces;
 
-namespace Prismatic.Domain.Repositories
+namespace Prismatic.Infra.Repositories
 {
     public class MatchRepository : MongoRepository<Match>, IMatchRepository
     {
-        protected override string CollectionName => "MatchCollection";
-
         public MatchRepository(IMongoClient mongoClient) : base(mongoClient)
         {
 
