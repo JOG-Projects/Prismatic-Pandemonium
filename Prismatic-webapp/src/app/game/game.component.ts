fa63f30ff32aa90c05118d85c12b0378c59ffc4b
DanielZanoniSantos <danielzanoni2001@gmail.com>
2023-03-05 06:49:42 -0300

    Pitagoras is the man

--- Prismatic-webapp/src/app/game/game.component.ts
+++ Prismatic-webapp/src/app/game/game.component.ts
@@ -7,13 +7,6 @@ import { Component, ViewChild } from '@angular/core';
 })
 export class GameComponent {
 
-  public moveCard(ev: MouseEvent) {
-    let card = ev.target as HTMLElement
-    card.style.position = 'absolute'
-    card.style.transform = "translateY(-300px) translateX(-300px)"
-    card.style.rotate = "360deg"
-  }
-
   deck = [
     "../assets/blue0.png",
     "../assets/red12.png",
@@ -24,5 +17,33 @@ export class GameComponent {
     "../assets/yellow4.png"
   ]
 
+  public moveCard(ev: MouseEvent) {
+    const cardsContainer = document.getElementById('player_hand')
+    // const cards = Array.from(cardsContainer!.querySelectorAll<HTMLElement>('.card'))
+    const cards = document.querySelectorAll<HTMLElement>('.card')
+
+    cards.forEach(card => {
+      card.addEventListener('click', () => {
+      const rect = card.getBoundingClientRect();
+      const cartaX = rect.left + rect.width / 2;
+      const cartaY = rect.top + rect.height / 2;
+
+      const distanciaX = 725 - cartaX;
+      const distanciaY = 381 - cartaY;
+      const distancia = Math.sqrt(distanciaX * distanciaX + distanciaY * distanciaY);
+
+      card.style.rotate = "360deg"
+      card.style.transition = 'all 0.3s ease-in-out' 
+      //tentar aplicar aq msm items para centralizar as cartas logo dps de serem jogadas
+      //dps fazer nick de cada jogador e intefarce ao lado das cartas
+      card.style.transform = `translate(${distanciaX}px, ${distanciaY}px)`;
+  })
+})
+    // let card = ev.target as HTMLElement
+    // card.style.position = 'absolute'
+    // card.style.transform = 'translateY(-377px) translateX(100px)'
+    // card.style.rotate = "360deg"
+  }
   @ViewChild("player_hand") player_hand: HTMLDivElement | null = null
+
 }
